cedb69df8c8b3965b9d4102e6b28012bdad34435
kraf24 <gpinchuk@gmail.com>
2024-03-12 18:33:56 +0100

    ex08 is done

---
+++ Module_08/ex01/Span.h
@@ -0,0 +1,16 @@
+//
+// Created by Марина Трещинская on 12.03.2024.
+//
+
+#ifndef CPP_MODULS_SPAN_H
+#define CPP_MODULS_SPAN_H
+
+
+
+class Span {
+
+};
+
+
+
+#endif //CPP_MODULS_SPAN_H
---
+++ Module_08/ex02/MutantStack.h
@@ -0,0 +1,16 @@
+//
+// Created by Марина Трещинская on 12.03.2024.
+//
+
+#ifndef CPP_MODULS_MUTANTSTACK_H
+#define CPP_MODULS_MUTANTSTACK_H
+
+
+
+class MutantStack {
+
+};
+
+
+
+#endif //CPP_MODULS_MUTANTSTACK_H
